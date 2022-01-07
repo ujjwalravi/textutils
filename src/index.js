b87796080bbfb23c54f90ceec2c32a597fa8e39f
ujjwalravi <59475060+ujjwalravi@users.noreply.github.com>
2022-01-07 18:01:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
    <App/>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
